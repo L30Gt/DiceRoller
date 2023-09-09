ad1a9647bfb7beb9db7abfa99068313d3ecc8e9d
L30Gt <leeo12322@hotmail.com>
2023-09-08 21:30:28 -0300

    feat: Implemented dice rolling logic

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -6,8 +6,25 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        buttonCalc.Clicked += OnRollButtonClicked;
+    }
 
-	
+    private void OnRollButtonClicked(object sender, EventArgs e)
+    {
+        if (diceList.SelectedIndex >= 0)
+        {
+            int numberSides = int.Parse(diceList.SelectedItem.ToString());
+            int result = RollDice(numberSides);
+
+            diceResult.Text = result.ToString();
+        }
+    }
+
+    private int RollDice(int numberSides)
+    {
+        Random random = new Random();
+        return random.Next(1, numberSides + 1);
+    }
+
 }
 

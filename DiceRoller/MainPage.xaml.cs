720d7d3b9f72287c87a8adce12ec83f2a4dab07f
L30Gt <leeo12322@hotmail.com>
2023-09-18 00:54:07 -0300

    feat: Created Dice class and a method to show a different dice image based on picker choice

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -3,28 +3,56 @@
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
-        buttonCalc.Clicked += OnRollButtonClicked;
+    public MainPage()
+    {
+        InitializeComponent();
+        buttonCalc.Clicked += ButtonClicked;
+
+        diceList.SelectedIndexChanged += PickerChanged;
     }
 
-    private void OnRollButtonClicked(object sender, EventArgs e)
+    public class Dice
     {
-        if (diceList.SelectedIndex >= 0)
+        public Dice (int numSides)
         {
-            int numberSides = int.Parse(diceList.SelectedItem.ToString());
-            int result = RollDice(numberSides);
+            this.numSides = numSides;
+        }
+        private int numSides;
 
-            diceResult.Text = result.ToString();
+        public int RollDice()
+        {
+            Random random = new Random();
+            return random.Next(1, numSides + 1);
         }
     }
 
-    private int RollDice(int numberSides)
+    private void ButtonClicked(object sender, EventArgs e)
     {
-        Random random = new Random();
-        return random.Next(1, numberSides + 1);
+        if (diceList.SelectedIndex >= 0)
+        {
+            int numSides = int.Parse(diceList.SelectedItem.ToString());
+
+            /*  Criando uma instância da classe Dice e atribuindo-a à variável dice
+                NomeDaClasse nomeDaVariavel = new NomeDaClasse(argumentos); */
+            Dice dice = new Dice(numSides);
+
+            int result = dice.RollDice();
+
+            diceResult.Text = result.ToString();
+
+            //diceImage.Source = ImageSource.FromFile($"dice_d{numSides}.png");
+        }
     }
 
+    private void PickerChanged(object sender, EventArgs e)
+    {
+        if (diceList.SelectedIndex >= 0)
+        {
+            int numSides = int.Parse(diceList.SelectedItem.ToString());
+
+            diceImage.Source = ImageSource.FromFile($"dice_d{numSides}.png");
+        }
+    }
 }
+
 
